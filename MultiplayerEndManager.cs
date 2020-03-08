34ab128cc791cb781a197568498a190924267912
tompedro <55707296+tompedro@users.noreply.github.com>
2020-03-08 12:46:53 +0100

    Change 'status' to 'state'

--- MultiplayerEndManager.cs
+++ MultiplayerEndManager.cs
@@ -13,6 +13,6 @@ public class MultiplayerEndManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = PlayerPrefs.GetInt("Status").ToString();
+        text.text = PlayerPrefs.GetInt("State").ToString();
     }
 }
